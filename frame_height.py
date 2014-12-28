bbf4b1b8a3d5ec3b5bb7c4ddd238b6ae53e47323
James Cherry <jcherry@auvik.com>
2014-12-27 21:18:01 -0500

    Improve frame height script
    - I now create the OptionMenu and Entry widgets at the same time, and
      simply use forget() to display one or the other.

--- frame_height.py
+++ frame_height.py
@@ -9,6 +9,7 @@
 # button be part of the frame, which can be enabled when the selection value
 # has been fixed.
 from Tkinter import *
+from Tkinter import _setit
 
 class ui(Frame):
 
@@ -19,6 +20,8 @@ class ui(Frame):
         self.f = dict()
         # For selection widgets inside frames.
         self.w = dict()
+        # For entry boxes, once a particular value has been selected.
+        self.e = dict()
         # For clear buttons inside frames.
         self.r = dict()
         # For StringVars.
@@ -34,30 +37,19 @@ class ui(Frame):
 
     def add_option_menu(self, k, row):
         # Have we created the frame yet? If not, create it.
-        if k not in self.f:
-            self.f[k] = Frame(self.parent, padx=5)
+        self.f[k] = Frame(self.parent, padx=5)
         f = self.f[k]
-        # If the variable already exists, destroy it.
-        if k in self.tv:
-            self.v[k].trace_vdelete('w', self.tv[k])
-            del(self.tv[k])
-        if k in self.v:
-            del(self.v[k])
         self.v[k] = StringVar(f, name=self._uname(k))
-        self.v[k].set('Select %s' % k)
-        self.tv[k] = self.v[k].trace('w', self.search_modified)
-        if k in self.w:
-            del(self.w[k])
-        self.w[k] = OptionMenu(f, self.v[k], *map(lambda x: '%s %d' % (k, x),
-            range(1, 4)))
-        self.w[k].grid(row=0, column=0, sticky='ew')
-        if k not in self.r:
-            self.r[k] = Button(f, text='Clear',
-                command=eval('self.clear_%s' % k))
-            self.r[k].grid(row=0, column=1)
-        self.r[k].configure(state='disabled')
+        self.v[k].set('')
+        self.w[k] = OptionMenu(f, self.v[k], self.v[k].get())
+        self.e[k] = Entry(f, disabledforeground='blue',
+            disabledbackground='#cdcdff')
+        self.r[k] = Button(f, text='Clear', command=eval('self.clear_%s' % k))
+        self.r[k].grid(row=0, column=1)
+        self.set_option_choices(k)
         f.grid(row=row, column=0, sticky='ew')
         f.columnconfigure(0, weight=1, minsize=230)
+        f.columnconfigure(1, minsize=70)
         f.rowconfigure(0, minsize=35)
 
     def clear_Make(self):
@@ -69,23 +61,43 @@ class ui(Frame):
     def clear_Year(self):
         self.search_modified('Clear', 'Year', 2)
 
+    def set_option_choices(self, k):
+        print "-- Now here with", k
+        self.e[k].grid_remove()
+        self.r[k].configure(state='disabled')
+        self.legal = ['Select %s' % k]
+        for i1 in range(3):
+            self.legal.append('%s %s' % (k, i1+1))
+        self.v[k].set(self.legal[0])
+        self.w[k]['menu'].delete(0, 'end')
+        for choice in self.legal:
+            self.w[k]['menu'].add_command(label=choice,
+                command=_setit(self.v[k], choice))
+        self.w[k].grid(row=0, column=0, sticky='ew')
+        self.tv[k] = self.v[k].trace('w', self.search_modified)
+
+    def set_entry(self, k):
+        print "-- Set entry", k
+        self.w[k].grid_remove()
+        self.r[k].configure(state='normal')
+        self.e[k].delete(0, 'end')
+        self.e[k].insert(0, self.v[k].get())
+        self.e[k].configure(state='disabled')
+        self.e[k].grid(row=0, column=0, sticky='ew')
+        self.v[k].trace_vdelete('w', self.tv[k])
+
     def search_modified(self, *args):
         # The modified OptionMenu is in args[0], but we've appended a nunber to
         # it. Get the key of the modified OptionMenu.
         cvar = args[0]
+        print "-- In search_modified", args
         if cvar == 'Clear':
-            k, row = args[1], args[2]
-            self.add_option_menu(k, row)
+            k = args[1]
+            print "-- Reset to", k
+            self.set_option_choices(k)
         else:
             k = cvar.split('_')[0]
-            self.r[k].configure(state='normal')
-            self.w[k].destroy()
-            del(self.w[k])
-            self.w[k] = Entry(self.f[k], disabledforeground='blue',
-                disabledbackground='#cdcdff')
-            self.w[k].insert(0, self.v[k].get())
-            self.w[k].configure({'state': 'disabled'})
-            self.w[k].grid(row=0, column=0, sticky='ew')
+            self.set_entry(k)
 
 root = Tk()
 self = ui(root)
